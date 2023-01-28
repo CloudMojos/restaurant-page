216398172e52a070fc9dce0533b88ba61d18d592
CloudMojos <joshuel.simbulan@gmail.com>
2023-01-28 20:12:34 +0800

    nav.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import defaultExport from "./nav";
 console.log('Hello world!')
 
 // - Run webpack server
 // - Use import export
 //   > for handling tab  navigation
 //   
+
+// 
+
+const content = document.querySelector('#content');
+
+content.appendChild(creatNav());
+
